69b500c00ff7245fab7c9188b9d554d00efbda17
DarinaTrifonova <darina.trifonova88@gmail.com>
2020-03-05 14:54:27 +0200

    comment so we have merge conflicts

--- Keywords/pricesStuff.py
+++ Keywords/pricesStuff.py
@@ -1,9 +1,12 @@
 def remove_dollar_sign_from_price(price):
     return price[1:]
 
+##comment from Dari :)
+
 
 def calculate_vat(subtotal):
     vat = 0.2
     vat_value = float(subtotal) * vat
 
     return round(vat_value, 2)
+
